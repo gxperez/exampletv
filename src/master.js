ff01a9947511fec55fcd3c2640c06d17ccde0c14
=Grequis perez <gperez@cnd.com.do>
2016-05-17 17:55:57 -0400

    Recursos e imagenes del ejercicio

--- src/master.js
+++ src/master.js
@@ -1,6 +1,5 @@
 /**  here is the class extension for master.js*/
 alert('master.js loaded');
-
 var widgetAPI = new Common.API.Widget();
 var tvKey = new Common.API.TVKeyValue();
 var pluginAPI = new Common.API.Plugin();
@@ -8,9 +7,9 @@ var fileSystemObj = new FileSystem();
 var fileObj = {};  
 var instancia = null;
 var gobalThemeChart = {};
-var configFiles = ["serverUrl.data", "version.data", "allsource.data", "log.data" ]; 
-//{ 0 = serverURL, 1 = version, 2 = all source } 
+var configFiles = ["serverUrl.data", "version.data", "allsource.data", "log.data" ];   //{ 0 = serverURL, 1 = version, 2 = all source } 
 
+/*** ECHART REQUIRED LIB  ***/
 require.config({
     paths: {
         echarts: 'src/eChart'
@@ -18,30 +17,27 @@ require.config({
 });
 
 require(['echarts/theme/shine'], function (tarTheme) {
-    gobalTheme = tarTheme;
+	gobalThemeChart = tarTheme;
 });
 
+/***** FIN ECHARTS LIB ******/
 Master = {		
 		KeyDown: function(){
 		var keyCode = event.keyCode;		
 			    if(instancia == null || instancia === undefined ){			    	
 			    	instancia = new MasterTV();			    	
-			    	instancia.handleKeyDown(keyCode);			    	
-			    	
+			    	instancia.handleKeyDown(keyCode);
 			    }else{			    	
 			    	instancia.handleKeyDown(keyCode);			        
 			    }
-		}, 		
+		},
 		
 		initt: function(){
 			 widgetAPI.sendReadyEvent();			
-		     document.getElementById("anchor_main").focus();	
-		     
+		     document.getElementById("anchor_main").focus();
 		     if(instancia == null || instancia === undefined ){			    	
 		    	 instancia = new MasterTV();		    	  
 		     }
-		     
-		     alert("Finalizo la COnfiguaracion");
 		    require(
 		            [
 		                'echarts',
@@ -51,107 +47,95 @@ Master = {
 		            ],
 		            function (ec) {
 		                // Initialize after dom ready
-		            	/*
-		                console.log(gobalTheme);             
+		            	/*             
 		                    var myChart = ec.init(document.getElementById("applicationWrapper"));           
 		                    myChart.setTheme(gobalTheme);
-		                    myChart.setOption(option);		                    
-		                    ggg = myChart;
+		                    myChart.setOption(option);	                    
 		            	 */
 		            }
 		            );		    
-		    // Instancia del Web Socket.
-		    
-		    alert("Estableciendo conexion con el webSocket");
-			Server = new FancyWebSocket('ws://10.234.133.76:9300'); 
-			//Let the user know we're connected
-			Server.bind('open', function() {
-				log( "Connected." );
-			});
-			//OH NOES! Disconnection occurred.
-			Server.bind('close', function( data ) {
-				log( "Disconnected." );
-			});
-			//Log any messages sent from server
-			Server.bind('message', function( payload ) {
-				log( payload );
-			});
+		},
+		
+		reconectar : function(){
 			
-	//		alert("Se va a conectar");
-			// Gananar un Hanckathon, no es parte de la Propuesta.
-			// 
+			if(instancia == null || instancia === undefined ){			    	
+		    	 instancia = new MasterTV();		    	  
+		     }		
+			instancia.conectar(true);			
+		},
+		
+		recetor: function(data){
 			
-			Server.connect();			
-			alert("LLego Aqui");			
-			// Ajuses Smart TV comentaro de Exhibicion.
-		}		
+						
+		}
 };
 
 
 MasterTV = function() {	
 	this.page_config = {};	
 	this.app_info = {server: "", version: ""};	
-	
+	this.ManagerPages = {}; 
 	var index;
 	var content_txt = [];
 	var content_img = [];
 	var title_txt = [];	
-	
-	
-	var sessionesPAG = ["applicationWrapper"];	
+	var sessionesPAG = ["applicationWrapper"];
 	
 	if(this.setFileConfig()){
-		alert("Se configuro correctamente...."); 
+		this.conn = new ConexionTV(this.app_info.server);
 	}	
-	widgetAPI.sendReadyEvent();
+	
+	widgetAPI.sendReadyEvent();	
+	
 };
 
 MasterTV.prototype.setFileConfig = function(){	
-	alert( "Ayunen: SetFileConfig()" ); 
+	alert( "SetFileConfig()" ); 
 	// Yes Exists File. OF COMPUTER.	
 	var firstData = ['10.234.133.76:9300'];
 	var app_info = {}; 
 	var page_config = {}; 	
 	
 	configFiles.forEach(function (item, index, array) {		
-		fileObjFirst = fileSystemObj.openCommonFile(curWidget.id + '/' + item , 'r');		
+		fileObjFirst = fileSystemObj.openCommonFile(curWidget.id + '/' + item , 'r');
 		if(!fileObjFirst){
-			alert("=== Se Escribe el Archivo en commun: " + item);
+			alert("=== Primer Uso de la Aplicacion. Se Escribe el Archivo en commun: " + item);
 			 var textA = ""; 
 			switch (index) {
-			case 0:  //   
-				alert("Configurando Lista de Serviodr"); 
+			case 0:  //
 				textA = firstData.toString();				
 				app_info.server = firstData;
-				
-				alert("===================== Jesus LLoro ===============");
 				break;				
 			case 1:  // { 0 = serverURL, 1 = version, 2 = all source }
-				textA = "1.0.0,2016-05-16";
-				textArr = textA.split(","); 
-				app_info.version = textArr[0];
-				app_info.fecha_modificacion = textArr[1];								
-				  
+				textA = "{version: '1.0.0', fecha_modificacion: '2016-05-16'}";
+				eval("textArr = " +  textA);
+				app_info.version = textArr.version;
+				app_info.fecha_modificacion = textArr.fecha_modificacion;
+				
+				alert(textA);
+				alert("======= La Version ==================="); 
 				break;			
 			case 2:  //
 				textA = "{ initt: function(option){  } }";				
 				page_config.source = textA;  
-				break;			
+				break;				
+			case 3:
+				textA = "/** Log.data **/";
+				break;				
 			default:
 			   textA = ""; 			
 			break; 
 			}
-			
 			var fileObjTemp = fileSystemObj.openCommonFile(curWidget.id + '/' + item, 'w');
 			fileObjTemp.writeAll(textA);
-			fileSystemObj.closeCommonFile(fileObjTemp);			
-		} else {	
+			fileSystemObj.closeCommonFile(fileObjTemp);
 			
-			alert("------------------------------------------ Ya existe el Documento Please no lo sobreescribas-----------"); 
+		} else {
+			alert("/**************** Existen Documentos. (Lectura de los Ficheros) **********/"); 
 			
 			var strLine = '';
 			var arrResult = new Array();
-			var allStr = ""; 
+			var allStr = "";
 			
 			while (strLine = fileObjFirst.readLine()) {
 			    arrResult.push(strLine);
@@ -161,45 +145,39 @@ MasterTV.prototype.setFileConfig = function(){
 			switch (index) {
 				case 0:  //		array de IPs			
 					app_info.server = allStr.split(",");
-					
 					break;				
-				case 1:  // { 0 = serverURL, 1 = version, 2 = all source }
-					
-					versionArr = allStr.split(","); 
-					app_info.version = textArr[0];
-					app_info.fecha_modificacion = textArr[1];					
+				case 1:  // { 0 = serverURL, 1 = version, 2 = all source }					
+
+					eval("versionArr = " + allStr);					
+					app_info.version = versionArr.version;
+					app_info.fecha_modificacion = versionArr.fecha_modificacion;					
 					break;
 					
 				case 2:  // Codigo de Programamacion
 					eval("page_config.source = " + allStr );					
-					alert(page_config.source);
 					
 					alert("La version del Codigo es: ");
 					alert(allStr);
-					
-					
 					break;					
-				default:
-					console.log("Has descuidado en tu vida" ); 
+				default:					
+					console.log("Nungun Archivo en Particular" ); 
 				break; 
 			}
-			
-			
-			
 		}
-	});
-	
+	});	
 		this.app_info = app_info;
 		this.app_config = page_config;
 		return true;
+};
+
+MasterTV.prototype.UpdateConfig = function(){	
 }; 
 
-MasterTV.prototype.setPagesConfig = function(){	
-	// Las tablas de diferencias entre los elementos.
-	
-	fileObj = fileSystemObj.openCommonFile(curWidget.id + '/testFile.data', 'w');
-	fileObj.writeAll('var js = "La sinfonica de Dios"; for(var i in js){ console.log(i); console.log(js[i]);  }');
-	fileSystemObj.closeCommonFile(fileObj);
+
+MasterTV.prototype.setPagesConfig = function () {
+};
+
+MasterTV.prototype.renderPage = function (tipoID) {
 	
 };
 
@@ -214,63 +192,97 @@ MasterTV.prototype.handleKeyDown = function (keyCode) {
 		case sf.key.UP:
 			--index;
 			if(index<0)index = index + 4;
-
 			break;
 		case sf.key.DOWN:
 			index++;
 			if(index>3)index = index - 4;			
 			break;
 		case sf.key.ENTER:
-			alert("Tecla Enter: Obtener Mac del TV:");
-			 var networkPlugin = document.getElementById('pluginNetwork');
-			 var mac = networkPlugin.GetMAC(0) || networkPlugin.GetMAC(1);						
-			alert("Envio de la Mack Adress al Servidor"); 
-				log( 'You: ' + mac );
-				send(mac);
-				/*
-				 * 
-				 * LA APLICACIóN SOPOPRTA MUY BIEN EL LOCALSTOGE.
-				 
-			if(typeof(Storage) !== "undefined") {				
-			    // Code for localStorage/sessionStorage.
-				alert("Este soporta y eso esta OK SI ");
-			//	localStorage.setItem("name", "Mi SUPER");				
-			} else { 
-			    // Sorry! No Web Storage support..
-				alert("Sorry! No Web Storage support..");				
-			}
-			*/				
-				// COMUN FILE CONTROLLER.
-				
 			break;
-		default:
-			alert("SIII Probando"); 
-			alert(localStorage.getItem("name"));		
-			alert("handle default key event, key code(" + keyCode + ")");
-			
-			alert("Aplicacion de Lectura");
-			var fileObj2 = fileSystemObj.openCommonFile(curWidget.id + '/testFile.data', 'r');
-			var strLine = '';
-			var arrResult = new Array();
-
-			while (strLine = fileObj2.readLine()) {
-			    arrResult.push(strLine);
-			    alert(strLine); 
-			}
-			
+		default:		 
+			alert(localStorage.getItem("name"));
 			break;
 	}
 };
- 
-/* EL websocket en el TV
- * **/
-var Server;
+
+
+var ConexionTV = function(listIp){
+	
+	alert("Instancia de la Conxion del Servidor");
+	this.fechaJson = new Date();	
+	this.Server = {}; 
+	
+	this.current_TV = {
+			clienteSessionID: 0, 
+			macAdreesTV: "", 
+			Tipo: 'TV', 	
+			hash: "",
+			fecha: "",
+			accion: "REP"
+		};
+	
+	this.ListIp = listIp; 
+	this.cIndex = 0;	
+	this.conectar(false);
+	
+	// REcorrido de listas de IPs
+		// Ajuses Smart TV comentaro de Exhibicion.
+};
+
+ConexionTV.prototype.conectar = function(cNext){
+	var cLen = this.ListIp.length;
+	var cArr = this.ListIp;
+	var cIndex = this.cIndex;
+	
+	var fechaJson = this.fechaJson.toJSON();
+	
+	if(cNext){
+		cIndex++;		
+		if(cIndex >= cLen ){
+			cIndex = 0; 
+		}
+	}	
+	
+	var ServerTEMP = new FancyWebSocket('ws://' + cArr[cIndex].toString().trim());	
+	this.Server = ServerTEMP;
+	//Let the user know we're connected
+	this.Server.bind('open', function() {
+	log( "Connected." );	
+	var networkPlugin = document.getElementById('pluginNetwork');
+	var mac = networkPlugin.GetMAC(0) || networkPlugin.GetMAC(1);
+	current_TV = {
+			clienteSessionID: 0,
+			macAdreesTV: mac,
+			Tipo: 'TV',
+			hash: "",
+			fecha: fechaJson,
+			accion: "ACTIVA"
+		};
+	ServerTEMP.send("message", current_TV);
+	});
+	
+	//OH NOES! Disconnection occurred.
+	this.Server.bind('close', function( data ) {
+		log( "Disconnected." );
+		var myVarReconect = setTimeout(function(){
+			Master.reconectar();						
+		}, 40000);			
+		
+				
+	});
+	//Log any messages sent from server
+	this.Server.bind('message', function( payload ) {		
+		Master.recetor(payload);
+		log( "Mensaje Recibido" );
+	});	
+	this.Server.connect();
+	this.cIndex = cIndex;
+}; 
+
 var content = $("<div id='log'></div>");
-
-
 function log( text ) {
 	//Add text to log
-	 content.html((content.html()?"\n":'')+text);
+	 content.html((content.html()?"\n":'')+ text );
 	 $.blockUI({
 	        message: content.html(),
 	        fadeIn: 700,
@@ -284,7 +296,7 @@ function log( text ) {
 	            left: 240 + 'px',
 	            border: 'none',
 	            padding: '15px',
-	            backgroundColor: '#000',
+	            backgroundColor: '#865',
 	            '-webkit-border-radius': '10px',
 	            '-moz-border-radius': '10px',
 	            opacity: 1,
@@ -293,12 +305,6 @@ function log( text ) {
 	    });	
 }
 
-function send( text ) {
-	Server.send( 'message', text );	
-}
-
 $( document ).ready(function() {
 	Master.initt();
-	alert("=================================");
-	
   });
