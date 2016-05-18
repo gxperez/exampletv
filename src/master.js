3455f2fb46f938a45ae79b6be1922e4902616bea
=Grequis perez <gperez@cnd.com.do>
2016-05-18 17:28:33 -0400

    Trabajando el Backend. Trabajando el Front end para recibir JSon y objetos

--- src/master.js
+++ src/master.js
@@ -21,7 +21,10 @@ require(['echarts/theme/shine'], function (tarTheme) {
 });
 
 /***** FIN ECHARTS LIB ******/
-Master = {		
+Master = {	
+		cuPage: "",
+		cuTheme: "",
+		$cuPage: {},
 		KeyDown: function(){
 		var keyCode = event.keyCode;		
 			    if(instancia == null || instancia === undefined ){			    	
@@ -52,24 +55,152 @@ Master = {
 		                    myChart.setTheme(gobalTheme);
 		                    myChart.setOption(option);	                    
 		            	 */
+		            	
+		            	// Prueba Renderizando el Html		            	
+		            	var op = Master.setOptionEsquema(3); 
+		            	/* {css:"inicio_02.css",
+		            			url: "template/inicio.html",
+		            			divID: undefined,
+		            			isAdd: null
+		            			};		            			
+		            	*/
+		            	
+		            	
+		            	
+		            	Master.renderPage(op);
+		            	
 		            }
 		            );		    
 		},
-		
-		reconectar : function(){
-			
+		reconectar : function(){			
 			if(instancia == null || instancia === undefined ){			    	
 		    	 instancia = new MasterTV();		    	  
 		     }		
 			instancia.conectar(true);			
-		},
-		
+		},		
 		recetor: function(data){
-			
-						
-		}
+		}		
 };
 
+Master.setOptionEsquema = function (esquema){
+	 var id = "#";
+	 var option = {css: "base.css", url: "template/base.html"};
+	 
+	switch(esquema){ 			
+		case 1:  //
+			id = "#aplication-Full";
+			option.divId = id; 
+		break; 			
+		
+		case 2:
+			id = "#aplication-02";
+			option.divId = id; 
+		break;
+
+		case 3:
+			id = "#aplication-03";
+			option.divId = id;
+		 			
+		break;
+
+	case 4:
+	// <div class='cs-div_V1x2'> </div>
+		id = "#aplication-04";
+		option.divId = id;		
+		break;
+		
+	case 5:
+		// 
+		id = "#aplication-05";
+		option.divId = id;
+		 
+		break;
+
+		case 6:
+			id = "#aplication-06";
+			option.divId = id;
+		break;
+
+		case 7:
+		// baseHtml = "<div class='cs-div_H1x2'>";
+			id = "#aplication-07";
+			option.divId = id;		 
+		break;
+
+		case 8:
+			id = "#aplication-08";
+			option.divId = id;
+		break;
+
+		case 9:
+			id = "#aplication-09";
+			option.divId = id;			 			 				
+		break;
+
+		default:			
+			option.url = "template/inicio.html"; 
+			option.divId = undefined;
+		break;
+	}
+	
+	return option; 
+	
+}; 
+
+
+Master.renderPage = function(option, callfunctionBack){
+	//  { c s s : "", u r l: "", divId: ""}	
+	Master.setTheme(option.css);
+	Master.setPage(option, callfunctionBack); 
+	// 
+};
+
+Master.setPage = function(option, callfunctionBack){
+	var isAdd = option.isAdd;	
+	var html = "";
+	
+	if(typeof callfunctionBack === 'undefined' ){
+		callfunctionBack = function(){}; 
+	}
+	
+	$.get(option.url, function( data ) {
+		Master.cuPage = data;	
+		Master.$cuPage = $(data);		
+		html = data;
+		 
+		if( typeof(option.divId) !== 'undefined'){
+			html = Master.$cuPage.find(option.divId).html();
+		}
+		
+		if(typeof isAdd === 'undefined' ||isAdd === null){
+			alert("#html DIV");
+			alert(html);
+			alert("FINAL SIP"); 
+			$("#applicationWrapper").html(html);
+			
+			callfunctionBack();
+			
+			return true; 
+		}	
+		if(isAdd == true){
+			alert("Apennd DIV");
+			$("#applicationWrapper").append(html);
+			callfunctionBack();
+			return true; 
+		}
+		
+		$("#applicationWrapper").html(html);
+		callfunctionBack();
+		return true; 		
+	});
+};
+
+Master.setTheme = function(file){	
+	$.get("template/styles/" + file, function( data ) {
+		Master.cuTheme = data;
+		$("#cssApplicationWrapper").html("<style>" + Master.cuTheme + "</style>");
+	});	
+};
 
 MasterTV = function() {	
 	this.page_config = {};	
@@ -90,7 +221,7 @@ MasterTV = function() {
 };
 
 MasterTV.prototype.setFileConfig = function(){	
-	alert( "SetFileConfig()" ); 
+	
 	// Yes Exists File. OF COMPUTER.	
 	var firstData = ['10.234.133.76:9300'];
 	var app_info = {}; 
@@ -170,6 +301,10 @@ MasterTV.prototype.setFileConfig = function(){
 		return true;
 };
 
+MasterTV.prototype.GetPages = function(url){
+	
+};
+
 MasterTV.prototype.UpdateConfig = function(){	
 }; 
 
@@ -178,14 +313,36 @@ MasterTV.prototype.setPagesConfig = function () {
 };
 
 MasterTV.prototype.renderPage = function (tipoID) {
-	
 };
+
+var indeces = 1;
+var it = 0; 
 
 MasterTV.prototype.handleKeyDown = function (keyCode) {
 	alert("SceneMainPage.handleKeyDown(" + keyCode + ")");
 	// TODO : write an key event handler when this scene get focused	
 	switch (keyCode) {
 		case sf.key.LEFT:
+			
+			var arry = ['<img src="template/img/samsungsetup.jpg" style="max-width: 1280px; max-height: 700px;">', 
+			            '<img src="template/img/indice.jpg" style="max-width: 1280px; max-height: 700px;">',
+			            '<img src="template/img/karthic.c.d.jpg" style="max-width: 1280px; max-height: 700px;">']; 
+			
+			var full = Master.setOptionEsquema(1);			
+        	Master.renderPage(full, function(){
+        		
+        		alert("Antes de********* "); 
+            	alert(arry[it]);
+            	$("#sc-full").html(arry[it]);        	
+            	it++;
+            	if(it > 2){
+            		it =0; 
+            	}
+            	
+        	});
+        	
+        		 
+			
 			break;
 		case sf.key.RIGHT:
 			break;
@@ -193,25 +350,35 @@ MasterTV.prototype.handleKeyDown = function (keyCode) {
 			--index;
 			if(index<0)index = index + 4;
 			break;
-		case sf.key.DOWN:
+		case sf.key.DOWN:			
+			if (indeces >= 10){
+				indeces = 0; 				
+			}			
+			
+			var op = Master.setOptionEsquema(indeces);
+        	Master.renderPage(op);
+        	
+        	indeces++; 
 			index++;
+			
 			if(index>3)index = index - 4;			
+			
 			break;
 		case sf.key.ENTER:
 			break;
-		default:		 
+		default:
+			
 			alert(localStorage.getItem("name"));
+		
 			break;
 	}
 };
 
 
-var ConexionTV = function(listIp){
-	
+var ConexionTV = function(listIp){	
 	alert("Instancia de la Conxion del Servidor");
 	this.fechaJson = new Date();	
-	this.Server = {}; 
-	
+	this.Server = {};
 	this.current_TV = {
 			clienteSessionID: 0, 
 			macAdreesTV: "", 
@@ -220,11 +387,9 @@ var ConexionTV = function(listIp){
 			fecha: "",
 			accion: "REP"
 		};
-	
 	this.ListIp = listIp; 
 	this.cIndex = 0;	
 	this.conectar(false);
-	
 	// REcorrido de listas de IPs
 		// Ajuses Smart TV comentaro de Exhibicion.
 };
